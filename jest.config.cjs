8f59d2897ddef27da28c742be4fc418509a6a500
mikepambo <mikepambo@gmail.com>
2025-07-05 11:47:21 +0200

    Remove deprecated test configuration and simplify Jest setup

--- jest.config.cjs
+++ jest.config.cjs
@@ -3,18 +3,7 @@ module.exports = {
   extensionsToTreatAsEsm: ['.ts'],
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
-  testMatch: ['**/__tests__/**/*.test.ts'],
-  transform: {
-    '^.+\\.tsx?$': ['ts-jest', {
-      useESM: true,
-      tsconfig: 'tsconfig.test.json'
-    }],
-  },
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
-  collectCoverageFrom: [
-    'src/**/*.{ts,tsx}',
-    '!src/**/*.d.ts',
-  ],
 };
